acae3adc09261b97925913bb48bf5791e2bb5add
leogarcialp <leogarcia986@gmail.com>
2023-05-09 01:03:40 -0600

    items fetch correctly. Working

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const moviesContainer = document.querySelector('#container');
+
 const loadMovies = async () => {
    
    try {
@@ -7,7 +9,29 @@ const loadMovies = async () => {
       // Check if the response is correct
       if(response.status === 200) {
          const data = await response.json();
-         console.log(data.results);
+
+         let movies = '';
+
+         data.results.forEach(movie => {
+            movies+= `
+            <div> 
+               <div>
+                  <img class="w-full" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="movie or serie cover">
+               </div>
+               <div class="text-sm md:text-base">
+                  <p class="mt-1 mb-5 font-semibold">${movie.title}</p>
+                  <div class="flex justify-between">
+                     <p class="">${movie.release_date}</p>
+                     <span class="px-0.5 border text-xs md:text-sm">Movie</span>
+                  </div>
+               </div>
+            </div> <!--card-->
+            `;
+         });
+
+         // Place the movies in the HTML
+         moviesContainer.innerHTML = movies;
+
       } else if (response.status === 401) {
          console.log('Your key is not right lol');
       } else if(response === 404) {
